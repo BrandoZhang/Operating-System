8430d534cf9571cc16a78705c0d67d1839a15b70
Brando Zhang <116010308@link.cuhk.edu.cn>
2019-05-20 22:01:46 +0800

    Add: Problem description and program template for Assignment 2

---
+++ 2. Multi-Thread Programming/source/hw2.cpp
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <curses.h>
+#include <termios.h>
+#include <fcntl.h>
+
+#define ROW 10
+#define COLUMN 50 
+
+
+struct Node{
+	int x , y; 
+	Node( int _x , int _y ) : x( _x ) , y( _y ) {}; 
+	Node(){} ; 
+} frog ; 
+
+
+char map[ROW+10][COLUMN] ; 
+
+// Determine a keyboard is hit or not. If yes, return 1. If not, return 0. 
+int kbhit(void){
+	struct termios oldt, newt;
+	int ch;
+	int oldf;
+
+	tcgetattr(STDIN_FILENO, &oldt);
+
+	newt = oldt;
+	newt.c_lflag &= ~(ICANON | ECHO);
+
+	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+	oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+
+	fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+	ch = getchar();
+
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+	fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+	if(ch != EOF)
+	{
+		ungetc(ch, stdin);
+		return 1;
+	}
+	return 0;
+}
+
+
+void *logs_move( void *t ){
+
+	/*  Move the logs  */
+
+
+	/*  Check keyboard hits, to change frog's position or quit the game. */
+
+	
+	/*  Check game's status  */
+
+
+	/*  Print the map on the screen  */
+
+	
+}
+
+int main( int argc, char *argv[] ){
+
+	// Initialize the river map and frog's starting position
+	memset( map , 0, sizeof( map ) ) ;
+	int i , j ; 
+	for( i = 1; i < ROW; ++i ){	
+		for( j = 0; j < COLUMN - 1; ++j )	
+			map[i][j] = ' ' ;  
+	}	
+
+	for( j = 0; j < COLUMN - 1; ++j )	
+		map[ROW][j] = map[0][j] = '|' ;
+
+	for( j = 0; j < COLUMN - 1; ++j )	
+		map[0][j] = map[0][j] = '|' ;
+
+	frog = Node( ROW, (COLUMN-1) / 2 ) ; 
+	map[frog.x][frog.y] = '0' ; 
+
+	//Print the map into screen
+	for( i = 0; i <= ROW; ++i)	
+		puts( map[i] );
+
+
+	/*  Create pthreads for wood move and frog control.  */
+
+	
+	/*  Display the output for user: win, lose or quit.  */
+
+	return 0;
+
+}
