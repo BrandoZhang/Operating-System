d3da0ac272917a08b272521917315b6ceb973fc2
Brando Zhang <116010308@link.cuhk.edu.cn>
2019-05-20 22:03:01 +0800

    Add: Problem description and program template for Assignment 5

---
+++ 5. I:O System Simulation/source/main.c
@@ -0,0 +1,152 @@
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/stat.h>
+#include <linux/fs.h>
+#include <linux/workqueue.h>
+#include <linux/sched.h>
+#include <linux/interrupt.h>
+#include <linux/slab.h>
+#include <linux/cdev.h>
+#include <linux/delay.h>
+#include <asm/uaccess.h>
+#include "ioc_hw5.h"
+
+MODULE_LICENSE("GPL");
+
+#define PREFIX_TITLE "OS_AS5"
+
+
+// DMA
+#define DMA_BUFSIZE 64
+#define DMASTUIDADDR 0x0        // Student ID
+#define DMARWOKADDR 0x4         // RW function complete
+#define DMAIOCOKADDR 0x8        // ioctl function complete
+#define DMAIRQOKADDR 0xc        // ISR function complete
+#define DMACOUNTADDR 0x10       // interrupt count function complete
+#define DMAANSADDR 0x14         // Computation answer
+#define DMAREADABLEADDR 0x18    // READABLE variable for synchronize
+#define DMABLOCKADDR 0x1c       // Blocking or non-blocking IO
+#define DMAOPCODEADDR 0x20      // data.a opcode
+#define DMAOPERANDBADDR 0x21    // data.b operand1
+#define DMAOPERANDCADDR 0x25    // data.c operand2
+void *dma_buf;
+
+// Declaration for file operations
+static ssize_t drv_read(struct file *filp, char __user *buffer, size_t, loff_t*);
+static int drv_open(struct inode*, struct file*);
+static ssize_t drv_write(struct file *filp, const char __user *buffer, size_t, loff_t*);
+static int drv_release(struct inode*, struct file*);
+static long drv_ioctl(struct file *, unsigned int , unsigned long );
+
+// cdev file_operations
+static struct file_operations fops = {
+      owner: THIS_MODULE,
+      read: drv_read,
+      write: drv_write,
+      unlocked_ioctl: drv_ioctl,
+      open: drv_open,
+      release: drv_release,
+};
+
+// in and out function
+void myoutc(unsigned char data,unsigned short int port);
+void myouts(unsigned short data,unsigned short int port);
+void myouti(unsigned int data,unsigned short int port);
+unsigned char myinc(unsigned short int port);
+unsigned short myins(unsigned short int port);
+unsigned int myini(unsigned short int port);
+
+// Work routine
+static struct work_struct *work_routine;
+
+// For input data structure
+struct DataIn {
+    char a;
+    int b;
+    short c;
+} *dataIn;
+
+
+// Arithmetic funciton
+static void drv_arithmetic_routine(struct work_struct* ws);
+
+
+// Input and output data from/to DMA
+void myoutc(unsigned char data,unsigned short int port) {
+    *(volatile unsigned char*)(dma_buf+port) = data;
+}
+void myouts(unsigned short data,unsigned short int port) {
+    *(volatile unsigned short*)(dma_buf+port) = data;
+}
+void myouti(unsigned int data,unsigned short int port) {
+    *(volatile unsigned int*)(dma_buf+port) = data;
+}
+unsigned char myinc(unsigned short int port) {
+    return *(volatile unsigned char*)(dma_buf+port);
+}
+unsigned short myins(unsigned short int port) {
+    return *(volatile unsigned short*)(dma_buf+port);
+}
+unsigned int myini(unsigned short int port) {
+    return *(volatile unsigned int*)(dma_buf+port);
+}
+
+
+static int drv_open(struct inode* ii, struct file* ff) {
+	try_module_get(THIS_MODULE);
+    	printk("%s:%s(): device open\n", PREFIX_TITLE, __func__);
+	return 0;
+}
+static int drv_release(struct inode* ii, struct file* ff) {
+	module_put(THIS_MODULE);
+    	printk("%s:%s(): device close\n", PREFIX_TITLE, __func__);
+	return 0;
+}
+static ssize_t drv_read(struct file *filp, char __user *buffer, size_t ss, loff_t* lo) {
+	/* Implement read operation for your device */
+	return 0;
+}
+static ssize_t drv_write(struct file *filp, const char __user *buffer, size_t ss, loff_t* lo) {
+	/* Implement write operation for your device */
+	return 0;
+}
+static long drv_ioctl(struct file *filp, unsigned int cmd, unsigned long arg) {
+	/* Implement ioctl setting for your device */
+	return 0;
+}
+
+static void drv_arithmetic_routine(struct work_struct* ws) {
+	/* Implement arthemetic routine */
+}
+
+static int __init init_modules(void) {
+    
+	printk("%s:%s():...............Start...............\n", PREFIX_TITLE, __func__);
+
+	/* Register chrdev */ 
+
+	/* Init cdev and make it alive */
+
+	/* Allocate DMA buffer */
+
+	/* Allocate work routine */
+
+	return 0;
+}
+
+static void __exit exit_modules(void) {
+
+	/* Free DMA buffer when exit modules */
+
+	/* Delete character device */
+
+	/* Free work routine */
+
+
+	printk("%s:%s():..............End..............\n", PREFIX_TITLE, __func__);
+}
+
+module_init(init_modules);
+module_exit(exit_modules);
