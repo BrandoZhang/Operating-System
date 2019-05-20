372ce2a5e34993a3dd681dbcb24e4dd34565fe14
Brando Zhang <116010308@link.cuhk.edu.cn>
2019-05-20 23:38:54 +0800

    Update: add submitted version of program in assignment 1. Note: no bonus program included (Hint: pipeline/shared memory/file operation).

--- 1. Kernel Mode Programming/source/program1/program1.c
+++ 1. Kernel Mode Programming/source/program1/program1.c
@@ -5,15 +5,150 @@
 #include <sys/types.h>
 #include <signal.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
 
-	/* fork a child process */
-	
-	/* execute test program */ 
-	
-	/* wait for child process terminates */
-	
-	/* check child process'  termination status */
-	
-	
+    int status;
+    /* fork a child process */
+    printf("Process start to fork\n");
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        perror("Fork error");
+        exit(EXIT_FAILURE);
+    } else {
+
+        // Child process
+        if (pid == 0) {
+            printf("I'm the child process, my pid = %d\n", getpid());
+
+            int i;
+            char *arg[argc];
+
+            for (i = 0; i < argc - 1; i++) {
+                // Get the command line variable except the first one
+                arg[i] = argv[i + 1];
+            }
+            arg[argc - 1] = NULL;
+
+            /* execute test program */
+            printf("Child process start to execute the program\n");
+            execve(arg[0], arg, NULL);
+
+            // Check if the child process is replaced by new program
+            printf("Continue to run original child process!\n");
+
+            perror("execve");
+            exit(EXIT_FAILURE);
+        }
+
+            // Parent process
+        else {
+            printf("I'm the parent process, my pid = %d\n", getpid());
+
+            /* wait for child process terminates */
+//            wait(&status);
+            waitpid(pid, &status, WUNTRACED);
+            printf("Parent process receiving the SIGCHLD signal\n");
+
+            /* check child process'  termination status */
+            if (WIFEXITED(status)) {
+                printf("Normal termination with EXIT STATUS = %d\n", WEXITSTATUS(status));
+            } else if (WIFSIGNALED(status)) {
+                int child_term_sign = WTERMSIG(status);
+                switch (child_term_sign) {
+                    case SIGABRT: {
+                        printf("child process get SIGABRT signal\n");
+                        printf("child process is terminated by abort signal\n");
+                        break;
+                    }
+                    case SIGALRM: {
+                        printf("child process get SIGALRM signal\n");
+                        printf("child process is terminated by alarm signal\n");
+                        break;
+                    }
+                    case SIGBUS: {
+                        printf("child process get SIGBUS signal\n");
+                        printf("child process is terminated by bus signal\n");
+                        break;
+                    }
+                    case SIGFPE: {
+                        printf("child process get SIGFPE signal\n");
+                        printf("child process is terminated by floating signal\n");
+                        break;
+                    }
+                    case SIGHUP: {
+                        printf("child process get SIGHUP signal\n");
+                        printf("child process is terminated by hangup signal\n");
+                        break;
+                    }
+                    case SIGILL: {
+                        printf("child process get SIGILL signal\n");
+                        printf("child process is terminated by illegal instruction signal\n");
+                        break;
+                    }
+                    case SIGINT: {
+                        printf("child process get SIGINT signal\n");
+                        printf("child process is terminated by interrupt signal\n");
+                        break;
+                    }
+                    case SIGKILL: {
+                        printf("child process get SIGKILL signal\n");
+                        printf("child process is terminated by kill signal\n");
+                        break;
+                    }
+                    case SIGPIPE: {
+                        printf("child process get SIGPIPE signal\n");
+                        printf("child process is terminated by pipe signal\n");
+                        break;
+                    }
+                    case SIGQUIT: {
+                        printf("child process get SIGQUIT signal\n");
+                        printf("child process is terminated by quit signal\n");
+                        break;
+                    }
+                    case SIGSEGV: {
+                        printf("child process get SIGSEGV signal\n");
+                        printf("child process is terminated by invalid memory reference.\n");
+                        break;
+                    }
+                    case SIGTERM: {
+                        printf("child process get SIGTERM signal\n");
+                        printf("child process is terminated by termination signal\n");
+                        break;
+                    }
+                    case SIGTRAP: {
+                        printf("child process get SIGTRAP signal\n");
+                        printf("child process is terminated by trap signal\n");
+                        break;
+                    }
+                    default: {
+                        printf("CHILD EXECUTION FAILED: %d\n", WTERMSIG(status));
+                        break;
+                    }
+                }
+                printf("CHILD EXECUTION FAILED!!\n");
+
+            } else if (WIFSTOPPED(status)) {
+                int child_stop_signal = WSTOPSIG(status);
+                switch (child_stop_signal) {
+                    case SIGSTOP: {
+                        printf("child process get SIGSTOP signal\n");
+                        printf("child process stopped\n");
+                        break;
+                    }
+                    default: {
+                        printf("CHILD PROCESS STOPPED: %d\n", WSTOPSIG(status));
+                        break;
+                    }
+                }
+
+                printf("CHILD PROCESS STOPPED\n");
+            } else {
+                printf("CHILD PROCESS CONTINUED\n");
+            }
+            exit(EXIT_SUCCESS);
+        }
+    }
+
+    return 0;
 }
--- 1. Kernel Mode Programming/source/program2/program2.c
+++ 1. Kernel Mode Programming/source/program2/program2.c
@@ -9,47 +9,126 @@
 #include <linux/jiffies.h>
 #include <linux/kmod.h>
 #include <linux/fs.h>
+#include <linux/signal.h>
+// #include <linux/sched/signal.h>
 
 MODULE_LICENSE("GPL");
 
+static struct task_struct *task;
 
-//implement fork function
-int my_fork(void *argc){
-	
-	
-	//set default sigaction for current process
-	int i;
-	struct k_sigaction *k_action = &current->sighand->action[0];
-	for(i=0;i<_NSIG;i++){
-		k_action->sa.sa_handler = SIG_DFL;
-		k_action->sa.sa_flags = 0;
-		k_action->sa.sa_restorer = NULL;
-		sigemptyset(&k_action->sa.sa_mask);
-		k_action++;
-	}
-	
-	/* fork a process using do_fork */
-	
-	/* excute a test program in child process */
-	
-	/* wait until child process terminates */
-	
-	return 0;
+struct wait_opts
+{
+    enum pid_type wo_type;
+    int wo_flags;
+    struct pid *wo_pid;
+
+    struct siginfo __user *wo_info;
+    int __user *wo_stat;
+    struct rusage __user *wo_rusage;
+
+    wait_queue_t child_wait;
+    int notask_error;
+};
+
+extern long _do_fork(unsigned long clone_flags,
+                     unsigned long stack_start,
+                     unsigned long stack_size,
+                     int __user *parent_tidptr,
+                     int __user *child_tidptr,
+                     unsigned long tls);
+
+extern long do_wait(struct wait_opts *wo);
+
+extern int do_execve(struct filename *,
+                     const char __user *const __user *,
+                     const char __user *const __user *);
+
+extern struct filename *getname(const char __user *);
+
+const char *IPC_SIG[23] = {  // IPC Signal name with correspoding number in Linux
+    "0", "SIGHUP", "SIGINT", "SIGQUIT", "SIGILL",
+    "SIGTRAP", "SIGABRT", "SIGBUS", "SIGFPE",
+    "SIGKILL", "SIGUSR1", "SIGSEGV", "SIGUSR2",
+    "SIGPIPE", "SIGALRM", "SIGTERM", "SIGSTKFLT",
+    "SIGCHLD", "SIGCONT", "SIGSTOP", "SIGSTP",
+    "SIGTTIN", "SIGTTOU"};
+
+/* function of child process    */
+unsigned long DoMyExecve(void *argc)
+{
+    int flag;
+    const char __user *filename = "/media/sf_CSC_3150_r/Ass/CSC3150_Assignment_1/source/program2/test";
+    // const char __user *filename = "./test";
+    printk("[program2] : child process");
+    flag = do_execve(getname(filename), NULL, NULL);  // execute test program
+    printk("[program2] : child process return %d\n", flag);
+    return 0;
 }
 
-static int __init program2_init(void){
+/* implement fork function  */
+int my_fork(void *argc)
+{
+    struct wait_opts wo;
+    int __user state;  // initialize state variable for wait_opts 
+    long pid;
 
-	printk("[program2] : Module_init\n");
-	
-	/* write your code here */
-	
-	/* create a kernel thread to run my_fork */
-	
-	return 0;
+    /* set default sigaction for current process */
+    int i;
+    struct k_sigaction *k_action = &current->sighand->action[0];
+    for (i = 0; i < _NSIG; i++)
+    {
+        k_action->sa.sa_handler = SIG_DFL;
+        k_action->sa.sa_flags = 0;
+        k_action->sa.sa_restorer = NULL;
+        sigemptyset(&k_action->sa.sa_mask);
+        k_action++;
+    }
+
+    /* fork a process using do_fork */
+    pid = _do_fork(SIGCHLD, (unsigned long)&DoMyExecve, 0, NULL, NULL, 0);
+
+    printk("[program2] : The child process has pid = %ld\n", pid);
+    printk("[program2] : This is the parent process, pid = %d\n", (int)current->pid);
+
+    /* initialize wait options	*/
+    wo.wo_type = PIDTYPE_PID;
+    wo.wo_flags = WEXITED;
+    wo.wo_pid = find_get_pid(pid);
+    wo.wo_info = NULL;
+    wo.wo_stat = &state;
+    wo.wo_rusage = NULL;
+
+    /* wait until child process terminates */
+    do_wait(&wo);
+    printk("get %s signal, signal number = %d\n", IPC_SIG[*wo.wo_stat], *wo.wo_stat);
+
+    return 0;
 }
 
-static void __exit program2_exit(void){
-	printk("[program2] : Module_exit\n");
+static int __init program2_init(void)
+{
+
+    printk("[program2] : Module_init\n");
+
+    /* write your code here */
+
+    /* create a kernel thread to run my_fork */
+    printk("[program2] : Module_init create kthread start\n");
+    task = kthread_create(&my_fork, NULL, "Program2ForkThread");
+
+    /* wake up new thread if ok */
+    if (!IS_ERR(task))
+    {
+        printk("[program2] : Module_init kthread starts\n");
+        wake_up_process(task);
+    }
+
+    return 0;
+}
+
+static void __exit program2_exit(void)
+{
+    printk("[program2] : Module_exit\n");
 }
 
 module_init(program2_init);
